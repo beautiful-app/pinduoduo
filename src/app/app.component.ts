29aea48ed377a4baa3c9078daba3ff5962091d28
1141957985@qq.com <chen432185731>
2020-10-13 16:36:44 +0800

    code backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TopMenu} from "../shared/entity/top-menu.vo";
+import {TOP_MEUNS} from "../shared/entity/const/top-menu.const";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pinduoduo';
+    title = 'pinduoduo';
+    topMenus: TopMenu[] = TOP_MEUNS;
 }
---
+++ src/shared/entity/const/top-menu.const.ts
@@ -0,0 +1,91 @@
+import {TopMenu} from "../top-menu.vo";
+
+export const TOP_MEUNS: TopMenu[] = [
+    {
+        title: '热门',
+        link: '',
+    },
+    {
+        title: '男装',
+        link: '',
+    },
+    {
+        title: '女装',
+        link: '',
+    },
+    {
+        title: '鞋子',
+        link: '',
+    },
+    {
+        title: '居家',
+        link: '',
+    },
+    {
+        title: '电器',
+        link: '',
+    }, {
+        title: '外卖',
+        link: '',
+    },
+    {
+        title: '服饰',
+        link: '',
+    },
+    {
+        title: '零食',
+        link: '',
+    }, {
+        title: '厨房',
+        link: '',
+    },
+    {
+        title: '飞行',
+        link: '',
+    }, {
+        title: '枪支',
+        link: '',
+    }, {
+        title: '雷管',
+        link: '',
+    },
+    {
+        title: '反舰导弹',
+        link: '',
+    },
+    {
+        title: '巡洋舰',
+        link: '',
+    },
+    {
+        title: '热门',
+        link: '',
+    },
+    {
+        title: '男装',
+        link: '',
+    },
+    {
+        title: '女装',
+        link: '',
+    },
+    {
+        title: '鞋子',
+        link: '',
+    },
+    {
+        title: '居家',
+        link: '',
+    },
+    {
+        title: '电器',
+        link: '',
+    }, {
+        title: '外卖',
+        link: '',
+    },
+    {
+        title: '服饰',
+        link: '',
+    },
+]
